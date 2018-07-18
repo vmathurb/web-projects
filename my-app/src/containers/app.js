ab659ed09de4a90ddfc7b637b47b7b3db9c3f05a
vmathurb <vishwanath.m@gmail.com>
2018-07-18 23:03:14 +0800

    Add switch for expanding or collapsing group level view

--- my-app/src/containers/app.js
+++ my-app/src/containers/app.js
@@ -4,6 +4,8 @@ import Divider from '@material-ui/core/Divider';
 import NavBar from '../components/navbar.js';
 import { withStyles, Paper } from '@material-ui/core';
 import SimpleTable from '../components/table';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const styles = theme => ({
   root: {
@@ -33,6 +35,19 @@ class App extends React.Component {
           </Grid>
           <Divider />
           <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Switch
+                  value="checkedA"
+                  classes={{
+                    switchBase: classes.colorSwitchBase,
+                    checked: classes.colorChecked,
+                    bar: classes.colorBar,
+                  }}
+                />
+              }
+              label="Expand"
+            />
             <SimpleTable />
           </Grid>
         </Grid>
